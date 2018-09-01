9ecd6404e5f2d3a6e2927eb61da882ce26d101dd
csjang <csjang@outloo.com>
2018-09-01 11:25:32 +0900

    add ignore file config

--- main.js
+++ main.js
@@ -1,46 +1,67 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+ 
 var app = http.createServer(function(req, res){
     var _url = req.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-
-    if(_url == '/'){
-        _url = '/index.html';
-    }
+    var pathname = url.parse(_url, true).pathname;
     
-    if(_url == '/favicon.ico'){
-        return res.writeHead(404);
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            var title = 'Welcome';
+            var description = 'Hello Node.js';
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${title}</h2>
+                <p>${description}</p>
+            </body>
+            </html>
+            `;
+            res.writeHead(200);
+            res.end(template);
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                res.writeHead(200);
+                res.end(template);
+            });
+        }
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
     }
-
-    res.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', function(err, data){
-        var template = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>>수업소개 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScri</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${data}</p>
-        </body>
-        </html>
-        `;
-
-        res.end(template);
-    });
-
+ 
 });
-
 app.listen(3003);
